d1a3c4bc7f81d1ca97f90ae5f5a2ba749d438daf
Miroslav Mihaylov <miro.mh@gmail.com>
2016-02-02 16:53:25 -0800

    minor changes

--- src/Aliens.java
+++ src/Aliens.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
 public class Aliens {
-
+	// * ALIENS
 	public static void main(String[] args) {
 		welcomeMsg();
 		Scanner input = new Scanner(System.in);
@@ -10,37 +10,37 @@ public class Aliens {
 		byte choise = 1;
 		do {
 
-			int tmp;
-
+			byte testsNumber;
+			// * number of tests
 			do {
 				System.out.println();
 				System.out.println("Number of tests (max 40): ");
-				tmp = input.nextInt();
-			} while (tmp < 1 || tmp > 40);
+				testsNumber = input.nextByte();
+			} while (testsNumber < 1 || testsNumber > 40);
+			// number of tests *
 
 			int testCount = 1;
-
-			for (int i = 0; i < tmp; i++) {
+			for (int i = 0; i < testsNumber; i++) {
 				System.out.println();
 				System.out.println("- Test number " + testCount + " -");
 
-				// word
+				// * word
 				String word = "";
 				do {
 					System.out.print("Enter word (min 2 symbols): ");
 					word = input.next().toLowerCase();
 				} while (word.length() < 2);
-				// end of word
+				// word *
 
-				// text
+				// * text
 				String text;
 				do {
 					System.out.print("Enter text: ");
 					text = input.next().toLowerCase();
 				} while (word.length() >= text.length() && text.length() < 10000);
-				// end of text
+				// text *
 
-				// print positions
+				// * print test results
 				System.out.println("Test result: ");
 				System.out.println(wordPosition(word, text));
 				int index = text.indexOf(word);
@@ -48,7 +48,7 @@ public class Aliens {
 					System.out.print(index + " ");
 					index = text.indexOf(word, index + 1);
 				}
-				// end of print positions
+				// print test results *
 
 				System.out.println();
 				testCount++;
@@ -57,10 +57,10 @@ public class Aliens {
 			continueOrExit();
 			choise = input.nextByte();
 		} while (choise != 0);
-		// *
+		// continue or exit *
 		endMsg();
 	}
-
+	// END OF ALIENS *
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	///////// Word Count /////////
@@ -81,7 +81,6 @@ public class Aliens {
 		System.out.println(" - - - A L I E N S - - -");
 		System.out.println("    -  -   - -   -  -   ");
 		System.out.println("      -           -     ");
-
 	}
 
 	public static void continueOrExit() {
@@ -90,13 +89,11 @@ public class Aliens {
 	}
 
 	public static void endMsg() {
-		for (int i = 0; i < 5; i++) {
-			System.out.println();
-		}
+		System.out.println("      |           |");
 		System.out.println("      -           -     ");
 		System.out.println("     Have a nice day !");
 		System.out.println("      -           -     ");
-
+		System.out.println("      |           |");
 	}
 
 }
